4f7b8aedbb1c5a9a0c44f388c6c3ec3ff3f5ec1e
Kinuthiaigiria <allanigiria@gmail.com>
2022-07-04 22:18:21 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,25 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                            
 
+                                                                                                                              
 
+                                                                                                                              
 
-int main(void)
+                                                                                                                              
 
-{
+int main(void)                                                                                                                
 
-	puts("\"Programming is like building a multilingual puzzle");
+	                                                                                                                              
 
-	return (0);
+{                                                                                                                             
 
-}
+	                                                                                                                              
+
+	        puts("\"Programming is like building a multilingual puzzle");                                                         
+
+		                                                                                                                              
+
+		        return (0);                                                                                                           
+
+			                                                                                                                              
+
+}  
