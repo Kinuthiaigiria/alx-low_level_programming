d013b447ab34fea6c5031beba89d582529feb6ff
Kinuthiaigiria <allanigiria@gmail.com>
2022-07-06 22:28:33 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
 */
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 24; i++)
 	{
@@ -33,7 +33,7 @@ void jack_bauer(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
-			_putchar('\n);
+			_putchar('\n');
 		}
 	}
 }
